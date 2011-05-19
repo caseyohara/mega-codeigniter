bf862c79c75fc03c0a4b7c085e9b47a4b8e8f0a2
Casey O'Hara <caseyo@caseyohara.com>
2011-05-19 00:19:25 -0600

    Move the environment out of the front controller and into a config.

---
+++ application/config/environment.php
@@ -0,0 +1,23 @@
+<?php
+
+define('ENVIRONMENT', 'development');
+
+if (defined('ENVIRONMENT'))
+{
+	switch (ENVIRONMENT)
+	{
+		case 'development':
+			error_reporting(E_ALL);
+		break;
+	
+		case 'testing':
+		case 'production':
+			error_reporting(0);
+		break;
+
+		default:
+			exit('The application environment is not set correctly.');
+	}
+}
+
+?>
