d22377b483aa0f773d290310d783bf8d2b999040
Casey O'Hara <caseyo@caseyohara.com>
2011-05-19 00:43:58 -0600

    Add more intuitive view structure that dries up a lot of controller code

--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -3,7 +3,7 @@
 class Welcome extends Application_Controller {
 
 	public function index() {
-		$this->load->view('welcome_message');
+		$this->render();
 	}
 	
 }
---
+++ application/views/layouts/application.html.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>CodeIgniter | Bare Install | Casey O'Hara</title>
+	<link rel="stylesheet" href="/css/application.master.css" type="text/css" />
+</head>
+<body>
+	
+	<?= $this->load->view($view) ?>
+
+</body>
+</html>
