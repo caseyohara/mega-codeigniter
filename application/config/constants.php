1ad7e932c2db9f0aba2f03b29c18d2d558d73724
Casey O'Hara <caseyo@caseyohara.com>
2011-05-19 00:20:19 -0600

    Add global IS_AJAX constant

--- application/config/constants.php
+++ application/config/constants.php
@@ -37,5 +37,17 @@ define('FOPEN_WRITE_CREATE_STRICT',				'xb');
 define('FOPEN_READ_WRITE_CREATE_STRICT',		'x+b');
 
 
+/*
+|--------------------------------------------------------------------------
+| AJAX
+|--------------------------------------------------------------------------
+|
+| Determines whether or not this request came from AJAX
+|
+*/
+define('IS_AJAX', isset($_SERVER['HTTP_X_REQUESTED_WITH']) &&
+ strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest');
+
+
 /* End of file constants.php */
 /* Location: ./application/config/constants.php */
